b3db6108ac578f84fae3fcca1a9165b95306caac
SimoneTomasella <simo64.tomasella@gmail.com>
2024-08-08 22:18:05 +0200

    Added parrot in ASCII art

---
+++ main.py
@@ -0,0 +1,24 @@
+def printParrot() :
+    print("                           .");             
+    print("                          | \\/|");
+    print("  (\\   _                  ) )|/|");                     
+    print("      (/            _----. /.'.'");
+    print(".-._________..      .' @ _\\  .'");
+    print("'.._______.   '.   /    (_| .')");
+    print("  '._____.  /   '-/      | _.'");
+    print("   '.______ (         ) ) \\ ");
+    print("     '..____ '._       )  )");
+    print("        .' __.--\\  , ,  // ((");
+    print("        '.'     |  \\/   (_.'(");
+    print("                '   \\ .'");
+    print("                 \\   (");
+    print("                  \\   '.");
+    print("                   \\ \\ '.)");
+    print("                    '-'-'");
+
+def main() :
+    printParrot();
+
+main();
+
+# Parrot by Morfina on https://www.asciiart.eu/animals/birds-land

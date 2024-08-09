bb3ce9ea6de3662c1e36ddd049e4bc2bb1dd0d46
SimoneTomasella <simo64.tomasella@gmail.com>
2024-08-09 21:47:43 +0200

    Added reading a string from terminal and starting to work on some argument checking

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def checkArguments() :
+    # Current version requres at least two arguments
+    argumentValues = 2
+
+    import sys
+    if (len(sys.argv) > argumentValues) :
+        sys.stderr.write("You can't use more than " + str(argumentValues) +  " arguments")
+        sys.exit(-1)
+    if (len(sys.argv) == 1) :
+        sys.stderr.write("Write something in the CLI!")
+        sys.exit(-1)
+
+
+def printString() :
+    import sys
+    print(sys.argv[1])
+
+
 def printParrot() :
     print("                           .");             
     print("                          | \\/|");
@@ -16,9 +34,12 @@ def printParrot() :
     print("                   \\ \\ '.)");
     print("                    '-'-'");
 
-def main() :
-    printParrot();
 
-main();
+def main() :
+    checkArguments()
+    printString()
+    printParrot()
+    
+main()
 
 # Parrot by Morfina on https://www.asciiart.eu/animals/birds-land
